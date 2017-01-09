84b53e71ac912de58fa19cda238391739d07a7df
Jakub Krystek <jakub.krystek@netbots.pl>
2017-01-09 22:00:33 +0100

    create RSAKeys mapper to store datas in keychain

--- SampleProject/KeychainWrapper.swift
+++ SampleProject/KeychainWrapper.swift
@@ -20,9 +20,56 @@ enum KeychainWrapperError:Error {
     case emptyPassword
 }
 
-class KeychainWrapper{
 
+fileprivate class RSAKeyMapper:NSCoding{
     
+    private(set)  var publicKey:(UInt, UInt) = (0,0)
+    private(set)  var privateKey:(UInt, UInt) = (0,0)
+    
+    
+    convenience init(publicKey:(UInt, UInt), privateKey:(UInt,UInt)) {
+        self.init()
+        
+        self.publicKey = publicKey
+        self.privateKey = privateKey
+        
+    }
+    
+    // MARK: - nsCoding protocol
+    
+    required convenience init?(coder aDecoder: NSCoder){
+        self.init()
+        
+        let publicKeyValue0 = aDecoder.decodeObject(forKey: "storeValueOfPublicKey0") as? UInt
+        let publicKeyValue1 = aDecoder.decodeObject(forKey: "storeValueOfPublicKey1") as? UInt
+        
+        
+        if let pubK0 = publicKeyValue0, let pubK1 = publicKeyValue1{
+            self.publicKey = (pubK0, pubK1)
+        }
+        
+        
+        let privateKeyValue0 = aDecoder.decodeObject(forKey: "storeValueOfPrivateKey0") as? UInt
+        let privateKeyValue1 = aDecoder.decodeObject(forKey: "storeValueOfPrivateKey1") as? UInt
+        
+        
+        if let privK0 = privateKeyValue0, let privK1 = privateKeyValue1{
+            self.privateKey = (privK0, privK1)
+        }
+        
+    }
+    
+    fileprivate func encode(with aCoder: NSCoder){
+        aCoder.encode(self.publicKey.0, forKey: "storeValueOfPublicKey0")
+        aCoder.encode(self.publicKey.1, forKey: "storeValueOfPublicKey1")
+        
+        aCoder.encode(self.privateKey.0, forKey: "storeValueOfPrivateKey0")
+        aCoder.encode(self.privateKey.1, forKey: "storeValueOfPrivateKey1")
+    }
+}
+
+
+class KeychainWrapper{
     
     private class func createKeychainItem(login:String) -> [String:Any]{
         
@@ -37,10 +84,10 @@ class KeychainWrapper{
     
     
     class func createAccount(login:String, password:String) throws -> Bool{
-
-        if login.characters.count == 0{
         
-        throw KeychainWrapperError.emptyLogin
+        if login.characters.count == 0{
+            
+            throw KeychainWrapperError.emptyLogin
         }
         
         if password.characters.count == 0{
@@ -63,19 +110,19 @@ class KeychainWrapper{
         
         keychainItem[kSecValueData as String] = passwordData
         status = SecItemAdd(keychainItem as CFDictionary, nil)
-
+        
         if status != errSecSuccess{
-        throw KeychainWrapperError.save("Save Error status code: \(status)")
+            throw KeychainWrapperError.save("Save Error status code: \(status)")
         }
-    
-    return true
+        
+        return true
     }
     
     
     
     class func deleteAccount(login:String) throws -> Bool{
-    
-       let keychainItem = KeychainWrapper.createKeychainItem(login: login)
+        
+        let keychainItem = KeychainWrapper.createKeychainItem(login: login)
         
         var status = SecItemCopyMatching(keychainItem as CFDictionary, nil)
         
@@ -86,11 +133,11 @@ class KeychainWrapper{
             if status != errSecSuccess{
                 throw KeychainWrapperError.delete("remove account error code: \(status)")
             }
-        return true
+            return true
         }
         else{
-        throw KeychainWrapperError.delete("account not exist: \(status)")
-         }
+            throw KeychainWrapperError.delete("account not exist: \(status)")
+        }
     }
     
     
@@ -111,14 +158,29 @@ class KeychainWrapper{
         let keychainItem = KeychainWrapper.createKeychainItem(login: login)
         var result:CFTypeRef?
         let status = SecItemCopyMatching(keychainItem as CFDictionary, &result)
-      
+        
         guard let keychainDic = result as? [String:Any] else{
             throw KeychainWrapperError.fetch("get user data error statusCode \(status)")
         }
         
-    let passwordData = keychainDic[kSecValueData as String] as! NSData
+        let passwordData = keychainDic[kSecValueData as String] as! NSData
         
+        return passwordData.isEqual(to: password.data(using: String.Encoding.utf8)!)}
+    
+    
+    
+    class func storageRSAKeys(publicKey:(UInt, UInt), privateKey:(UInt, UInt), forUser login:String) ->Bool{
         
+        var keychainItem = KeychainWrapper.createKeychainItem(login: login)
+        var status = SecItemCopyMatching(keychainItem as CFDictionary, nil)
         
-    return passwordData.isEqual(to: password.data(using: String.Encoding.utf8)!)}
+        if status == noErr{ //accout exist storage keys
+            let rsaKeyMapper = RSAKeyMapper(publicKey: publicKey, privateKey: privateKey)
+            let data = NSKeyedArchiver.archivedData(withRootObject: rsaKeyMapper)
+        }
+        else{
+            return false;
+        }
+        
+        return true}
 }
